dae9f141e5190df1ae7f9311af4b2ab3ec208080
gowtham-bonu-simform <gowtham@simformsolutions.com>
2023-04-28 19:02:34 +0530

    TE7-T133:ActiveJobBasices:Main:Refactor

--- app/sidekiq/reminder_job.rb
+++ app/sidekiq/reminder_job.rb
@@ -3,7 +3,7 @@ class ReminderJob
 
   def perform(*args)
     time_range_set = DateTime.now+5.hours
-    event = Event.where(event_time: time_range_set).take
+    event = Event.find_by_event_time(time_range_set)
     users = event.users.pluck(:email)
     ReminderMailer.with(users: users, event: event).notify_email.deliver_later
   end
